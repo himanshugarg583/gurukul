c923544a2fdcfae5a9bfae916ba573403fbf990d
himanshugarg583 <himanshu.garg@fonixtech.io>
2025-07-14 11:38:17 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/gurukul/', // 👈 Add your repo name here
+  base: '/gurukul/', // your repo name
+  plugins: [react()],
 })
